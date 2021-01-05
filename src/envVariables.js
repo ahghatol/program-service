d9df58648d6dd67b0f6b0d1cf914bb819ae35d48
Amol Ghatol <amol_g@techjoomla.com>
2021-01-05 12:22:06 +0530

    Issue #DP-1409 feat: Circle ci integration

--- src/envVariables.js
+++ src/envVariables.js
@@ -14,10 +14,10 @@ const envVariables = {
     DOCK_REDIS_PORT: process.env.dock_redis_port || 6379,
     SUNBIRD_AUTO_CREATION_TOPIC: process.env.sunbird_auto_creation_topic,
     config: {
-        user: process.env.sunbird_program_db_user || "postgres",
+        user: process.env.sunbird_program_db_user || process.env.POSTGRES_USER,
         host: process.env.sunbird_program_db_host || "localhost",
-        database: process.env.sunbird_program_db_name,
-        password: process.env.sunbird_program_db_password,
+        database: process.env.sunbird_program_db_name || process.env.POSTGRES_DB,
+        password: process.env.sunbird_program_db_password || process.env.POSTGRES_PASSWORD,
         port: process.env.sunbird_program_db_port || 5432,
         dialect: process.env.sunbird_program_db_dialect || "postgres",
         pool: {
@@ -31,4 +31,3 @@ const envVariables = {
     }
 }
 module.exports = envVariables;
-
